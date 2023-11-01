fdbbec62546e37a6fe3b43029f8fdcaba05dd27a
Rajan <rajanmasih154@gmail.com>
2023-11-01 14:35:22 +0530

    fix background

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Options from "./Components/Options";
 
 function App() {
   return (
-    <div className="bg-gradient-to-b from-blue-950 to-blue-700 p-4 flex flex-col justify-start items-center gap-8">
+    <div className="min-h-[100vh] bg-gradient-to-b from-blue-950 to-blue-700 p-4 flex flex-col justify-start items-center gap-8">
       <Header />
       <Options />
       <Outlet />
